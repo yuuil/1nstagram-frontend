5767cd9de163b6366d72f88b13c5bb8c397fa3ae
yuuil <yeyeah_1@daum.net>
2020-06-15 21:31:34 +0900

    📝 [ADD] Set up Apollo Client & Create First Query

---
+++ src/Apollo/Client.js
@@ -0,0 +1,19 @@
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { defaults, resolvers } from "./LocalState";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: "http://localhost:4000",
+});
+
+export default new ApolloClient({
+  cache,
+  link,
+  resolvers,
+});
+
+cache.writeData({
+  data: defaults
+});
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './Components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./Components/App";
+import { ApolloProvider } from "react-apollo-hooks";
+import Client from "./Apollo/Client";
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <ApolloProvider client={Client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
 );
-
