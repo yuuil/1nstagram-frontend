b3c641e1933778a6ca015fda340769feb8be6fd6
yuuil <yeyeah_1@daum.net>
2020-06-15 20:30:10 +0900

    📝 [ADD] Global Styles & Theme color

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {ThemeProvider} from "styled-components";
+import GlobalStyles from '../Styles/GlobalStyles';
+import Theme from '../Styles/Theme';
 
-function App() {
+const App = () => {
   return (
-    <div className="App" />
+    <ThemeProvider theme={Theme}>
+      <GlobalStyles />
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
